84981443c22eeda431de3a47ee5d7ecd452d4fea
Christopher Brown <228240024+Casper1007@users.noreply.github.com>
2026-02-05 06:59:20 -0600

    Delete Lab_1c/Bonus E/Lab 1c-Bonus E directory

--- Lab_1c/Bonus E/Lab 1c-Bonus E/1c_BonusC_outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bonus_c_route53_zone_id" {
-	description = "Route53 hosted zone ID for the app domain"
-	value       = local.route53_zone_id
-}
-
-output "bonus_c_app_url_https" {
-	description = "HTTPS URL for the app"
-	value       = "https://${local.app_fqdn}"
-}
